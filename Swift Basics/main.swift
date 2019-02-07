ee93b2bb293752e18152dbcd3d6cb7afdb04564c
Julia Wopata <julia.wopata@gmail.com>
2019-02-07 16:40:21 -0600

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Julia Wopata on 2/7/19.
+//  Copyright © 2019 Julia Wopata. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
